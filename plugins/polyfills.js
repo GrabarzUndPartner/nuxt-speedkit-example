fcf4f0f761410ed7592ef03898b2801a32f1a2c4
Thorn Walli <lammpee@gmail.com>
2021-04-07 20:35:53 +0200

    fix(polyfills): improve polyfill load

--- plugins/polyfills.js
+++ plugins/polyfills.js
@@ -1,20 +1,18 @@
-
-async function polfills(){
-
-  if (!('objectFit' in document.documentElement.style)) {
-    await import('object-fit-images')
-  }
-
-
-  if (!('HTMLPictureElement' in global || 'picturefill' in global)) {
-    await import('picturefill')
-    await import('picturefill/dist/plugins/mutation/pf.mutation.js')
-  }
+async function polyfills(){
 
   if (!('IntersectionObserver' in global)) {
-    import('intersection-observer')
+    await import('intersection-observer');
+  }
+
+  if (!('objectFit' in document.documentElement.style)) {
+    await import('object-fit-images');
+  }
+
+  if (!('HTMLPictureElement' in global || 'picturefill' in global)) {
+    await import('picturefill');
+    await import('picturefill/dist/plugins/mutation/pf.mutation.js');
   }
 
 }
 
-polfills();
+polyfills();
